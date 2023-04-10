5cbafd2de9f5d98810f0f73a1630f590e454dbd7
stoneddawg <st.kenneth93@gmail.com>
2023-04-10 02:52:38 -0400

    z offset notes
    negativeposition

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1534,7 +1534,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 38, 4, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -38, -4, 0 }//z offset set to 4.33
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
