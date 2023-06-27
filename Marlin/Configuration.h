aedf2166e24c5c33594c7a74a89ccc97b91021c6
stoneddawg <st.kenneth93@gmail.com>
2023-06-27 15:31:46 +0000

    update margin

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -116,7 +116,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Dirnoz 967c6a2"
+#define CUSTOM_MACHINE_NAME "Dirnoz 7a251dff"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -1534,7 +1534,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -38, -4, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -44.2, -13, 0 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
@@ -1544,7 +1544,7 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 10
+#define PROBING_MARGIN 15
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_FEEDRATE (50*60)
